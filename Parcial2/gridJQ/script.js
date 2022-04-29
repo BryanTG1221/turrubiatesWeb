e1cfbdca09c4ffb1ff40668162de24ef479efdba
BryanTurrubiates <bryanturrubiates@outlook.com>
2022-04-29 12:34:20 -0500

    Se agrego trabajo en clase

--- Parcial2/gridJQ/script.js
+++ Parcial2/gridJQ/script.js
@@ -1,11 +1,11 @@
-window.onload = function () {
+//window.onload = function () {
 
-    document.getElementById("contenedor").addEventListener("click",function(evento){
-        console.log(evento.target.id);
+    //document.getElementById("contenedor").addEventListener("click",function(evento){
+        //console.log(evento.target.id);
 
-        document.getElementById(evento.target.id).classList.toggle("box__target");
-    })
-}
+      //  document.getElementById(evento.target.id).classList.toggle("box__target");
+    //})
+//}
 
 $(document).ready(function(){
     $('#contenedor').click(function(e){
