4eb6e7916ea1c389ecba47a5850601a13f7b1a50
BryanTurrubiates <bryanturrubiates@outlook.com>
2022-05-03 21:09:35 -0500

    Se agrego la practica en clase Ajax

---
+++ Parcial3/PracticaAjax/peticion.php
@@ -0,0 +1,3 @@
+<?php
+    echo '!!FORZA FERRARI!!'
+?>
---
+++ Parcial3/PracticaAjax/scriptAjax.js
@@ -0,0 +1,49 @@
+
+    window.onload = function (){
+
+        var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+        var alertTrigger = document.getElementById('btn1')
+
+        function alert(message, type) {
+        var wrapper = document.createElement('div')
+        wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+        alertPlaceholder.append(wrapper)
+        }
+
+        if (alertTrigger) {
+        alertTrigger.addEventListener('click', function () {
+            alert('Nice, you triggered this alert message!', 'success')
+            })
+        }
+
+        console.log('Cargo el archivo');
+        
+
+        document.getElementById("btn1").addEventListener("click", function(){
+            document.getElementById("#btn1").show();
+        });
+        
+        function ajax() {
+            
+            const AjaxRequest = new XMLHttpRequest();
+            var url = 'peticion.php';
+
+            AjaxRequest.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log (this.responseText);
+                    document.getElementById("titulo").innerHTML = this.responseText;
+                }
+
+            }
+            AjaxRequest.open('GET',url,true);
+            AjaxRequest.send();
+            
+            
+        }
+        
+        document.getElementById("botonAjax").addEventListener("click", function(){
+            ajax();
+        })
+    }
+
